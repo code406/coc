fe4efc8b2ebda469139a0387c337a2e729209a3f
Code Not Acceptable <36109227+code406@users.noreply.github.com>
2018-10-19 13:07:15 +0200

    Still working on tiempos.c

--- tiempos.c
+++ tiempos.c
@@ -8,9 +8,10 @@
  * Fecha: 16-09-2017
  *
  */
-
+#include <time.h>
 #include "tiempos.h"
 #include "ordenacion.h"
+#include "permutaciones.h"
 
 /***************************************************/
 /* Funcion: tiempo_medio_ordenacion Fecha:         */
@@ -23,19 +24,66 @@ short tiempo_medio_ordenacion(pfunc_ordena metodo,
                               int N, 
                               PTIEMPO ptiempo)
 {
-/* vuestro codigo */
-}
+        struct timespec tp1, tp2;
+        clockid_t clk_id;
+        int** table = NULL;
+        int i, max = -1, min = -1;
+        
+        /* Genera tabla de permutaciones */
+        table = genera_permutaciones(n_perms, N);
+
+        
+        clk_id = CLOCK_MONOTONIC;
+        clock_gettime(clk_id, &tp1);        
+        /* realiza la ordenacion y obtiene num. max, min y total de OB */
+        for (i=0; i<n_perms; i++){
+                n = metodo(table[i], 0, N);
+
+                if(n>max) max=n;                
+                if (min = -1) min=n;
+                else if(n<min) min=n;
+                
+                tot = tot + n;
+        }
+        clock_gettime(clk_id, &tp2);
+
+
+        ptiempo->tiempo = (tp2.nsec - tp1.nsec)/n_perms;
+        ptiempo->n_elems= n_perms * N;                
+        ptiempo->medio_ob = tot/(ptiempo->n_elems);
+        ptiempo->min_ob = min;
+        ptiempo->max_ob = max;
+
+        return 0;
+}  
 
 /***************************************************/
 /* Funcion: genera_tiempos_ordenacion Fecha:       */
-/*                                                 */
-/* Vuestra documentacion                           */
+/*  escribe en fichero los tiempos medios, y los 
+números promedio, mı́nimo y máximo de veces que
+se ejecuta la OB con n perms permutaciones de tamaños
+en el rango desde num min hasta num max, incluidos, 
+usando incrementos de tamaño incr. Return ERR o OK*/
 /***************************************************/
 short genera_tiempos_ordenacion(pfunc_ordena metodo, char* fichero, 
                                 int num_min, int num_max, 
                                 int incr, int n_perms)
 {
-  /* vuestro codigo */
+        /*
+        FILE* f = NULL;
+        PTIEMPO ptiempo;
+
+        tiempo_medio_ordenacion(metodo, 10, 10, ptiempo);
+        
+        f = fopen ("file.txt", "w+");
+        
+        fprintf(f, "bla", ptiempo->n_elems, ptiempo->medio_ob, ptiempo->n_elems, ptiempo->n_elems);
+   
+
+        fclose(f);
+        */
+
+        return 0;
 }
 
 /***************************************************/
@@ -46,7 +94,5 @@ short genera_tiempos_ordenacion(pfunc_ordena metodo, char* fichero,
 /***************************************************/
 short guarda_tabla_tiempos(char* fichero, PTIEMPO tiempo, int n_tiempos)
 {
-  /* vuestro codigo */
+  return 0;
 }
-
-
