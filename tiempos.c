9d0a61abb6665efcb6f194968bd962e209959837
Code Not Acceptable <36109227+code406@users.noreply.github.com>
2018-10-23 23:58:06 +0200

    tiempos.c now works
    When running e5, values printed in file may suggest sth happens. Is SelectSort returning proper values?

--- tiempos.c
+++ tiempos.c
@@ -8,6 +8,11 @@
  * Fecha: 16-09-2017
  *
  */
+
+#define _POSIX_C_SOURCE 199309L
+
+#include <stdlib.h>
+#include <stdio.h>
 #include <time.h>
 #include "tiempos.h"
 #include "ordenacion.h"
@@ -19,70 +24,69 @@
 /* Vuestra documentacion (formato igual            */
 /* que en el primer apartado):                     */
 /***************************************************/
-short tiempo_medio_ordenacion(pfunc_ordena metodo, 
+short tiempo_medio_ordenacion(pfunc_ordena metodo,
                               int n_perms,
-                              int N, 
+                              int N,
                               PTIEMPO ptiempo)
 {
         struct timespec tp1, tp2;
         clockid_t clk_id;
         int** table = NULL;
-        int i, max = -1, min = -1;
-        
+        int i, max=-1, min=-1, tot=0, n=0;
+
         /* Genera tabla de permutaciones */
         table = genera_permutaciones(n_perms, N);
 
-        
         clk_id = CLOCK_MONOTONIC;
-        clock_gettime(clk_id, &tp1);        
-        /* realiza la ordenacion y obtiene num. max, min y total de OB */
+        clock_gettime(clk_id, &tp1);
+        /* realiza la ordenacion y obtiene num. total de OB */
         for (i=0; i<n_perms; i++){
                 n = metodo(table[i], 0, N);
-
-                if(n>max) max=n;                
-                if (min = -1) min=n;
+                if(n>max) max=n;
+                if (min==-1) min=n;
                 else if(n<min) min=n;
-                
-                tot = tot + n;
+                tot+=n;
         }
         clock_gettime(clk_id, &tp2);
 
-
-        ptiempo->tiempo = (tp2.nsec - tp1.nsec)/n_perms;
-        ptiempo->n_elems= n_perms * N;                
+        ptiempo->tiempo = (tp2.tv_nsec - tp1.tv_nsec)/n_perms;
+        ptiempo->n_elems= n_perms * N;
         ptiempo->medio_ob = tot/(ptiempo->n_elems);
         ptiempo->min_ob = min;
         ptiempo->max_ob = max;
 
         return 0;
-}  
+}
 
 /***************************************************/
 /* Funcion: genera_tiempos_ordenacion Fecha:       */
-/*  escribe en fichero los tiempos medios, y los 
+/*  escribe en fichero los tiempos medios, y los
 números promedio, mı́nimo y máximo de veces que
 se ejecuta la OB con n perms permutaciones de tamaños
-en el rango desde num min hasta num max, incluidos, 
+en el rango desde num min hasta num max, incluidos,
 usando incrementos de tamaño incr. Return ERR o OK*/
 /***************************************************/
-short genera_tiempos_ordenacion(pfunc_ordena metodo, char* fichero, 
-                                int num_min, int num_max, 
+short genera_tiempos_ordenacion(pfunc_ordena metodo, char* fichero,
+                                int num_min, int num_max,
                                 int incr, int n_perms)
 {
-        /*
         FILE* f = NULL;
-        PTIEMPO ptiempo;
+        PTIEMPO ptiempo = NULL;
+	int N;
 
-        tiempo_medio_ordenacion(metodo, 10, 10, ptiempo);
-        
-        f = fopen ("file.txt", "w+");
-        
-        fprintf(f, "bla", ptiempo->n_elems, ptiempo->medio_ob, ptiempo->n_elems, ptiempo->n_elems);
-   
+	ptiempo = (PTIEMPO) calloc(1, sizeof(TIEMPO));
+	if(!ptiempo) return -1;
 
+	f = fopen ("file.txt", "w");
+	if(!f) return -1;
+
+	for(N=num_min; N<=num_max; N+=incr){
+        	tiempo_medio_ordenacion(metodo, n_perms, N, ptiempo);
+		fprintf(f, "Con %d permutaciones de %d elementos --> Tiempo medio: %f, Minimo de OB: %d, Maximo de OB: %d, Media de OB: %f.\n",
+			n_perms, N, ptiempo->tiempo, ptiempo->min_ob, ptiempo->max_ob, ptiempo->medio_ob);
+	}
         fclose(f);
-        */
-
+	free(ptiempo);
         return 0;
 }
 
